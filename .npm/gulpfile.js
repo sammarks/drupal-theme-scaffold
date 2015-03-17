f028237c40b3252e355b144b60a0a48af2139703
Sam Marks <sam@sammarks.me>
2015-03-17 12:03:00 -0400

    Initial commit.

---
+++ .npm/gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var concat = require('gulp-concat-sourcemap');
+var sourcemaps = require('gulp-sourcemaps');
+
+var paths = {
+	scss: {
+		source: '../scss/default.scss',
+		watch: [
+			'../scss/**/*.scss',
+		]
+	},
+	css: {
+		source: [
+			// '../bower_components/package-name/style.css',
+			'../css/**/*.css'
+		]
+	},
+	font: {
+		source: [
+			// '../bower_components/package-fonts/fonts/*',
+			'../fonts/*'
+		]
+	},
+	js: {
+		source: [
+			'../js/*.js'
+		]
+	},
+	compiled: '../compiled'
+};
+
+gulp.task('scss', function() {
+	return sass(paths.scss.source, { sourcemap: true, trace: true })
+		.on('error', function (err) {
+			console.error('Error', err.message);
+		})
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.compiled));
+});
+
+gulp.task('css', ['scss'], function() {
+	gulp.src(paths.css.source)
+		.pipe(concat('css-concat.css'))
+		.pipe(gulp.dest(paths.compiled));
+});
+
+gulp.task('fonts', function() {
+	gulp.src(paths.font.source)
+		.pipe(gulp.dest(paths.compiled + '/fonts'));
+});
+
+gulp.task('js', function() {
+	gulp.src(paths.js.source)
+		.pipe(concat('default.js'))
+		.pipe(gulp.dest(paths.compiled));
+});
+
+gulp.task('watch', function() {
+	gulp.watch(paths.scss.watch, ['css']);
+	gulp.watch(paths.js.source, ['js']);
+	gulp.watch(paths.font.source, ['fonts']);
+});
+
+gulp.task('default', ['css', 'js', 'fonts', 'watch']);
+gulp.task('nowatch', ['css', 'js', 'fonts']);
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# drupal-theme-scaffold
-sammarks/scaffold adapted for Drupal themes.
+# Scaffold
+
+This repository is a basic scaffold for all templates I create. It uses the following technologies:
+
+- Bower
+- Gulp
+- Node.JS
+- SCSS
+
+Basically, it sets up the style folders and `gulpfile` to jumpstart creating a new mockup or template.
+
+To get started, all you have to do is `cd` to the `.npm` folder of the repository, and then type:
+
+	npm install
+	gulp
+	
+Gulp will start, compile all of your CSS assets, move fonts to where they're supposed to go, and
+then watch for any changes to your stylesheets, fonts, or Javascripts.
+
+## The .npm Folder
+
+The `.npm` folder is important because Drupal will pick up on .info files created by
+the node modules and try to load them as themes. If we put all the node modules inside
+a hidden folder, we don't run into this problem. The only caveat is that the `gulpfile`
+must go inside that folder as well.
